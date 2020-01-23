fa359cb727fd608c5801dc18db57db06d7bcc699
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-01-22 22:13:51 -0500

    handle colors escapes

--- toHtml.js
+++ toHtml.js
@@ -1,47 +1,32 @@
 'use strict'
-/*
-// https://en.wikipedia.org/wiki/ANSI_escape_code
-colors = ["black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"],
 
-_toHtml = content => {
-    // Assuming content is generated properly...
-    let htmlContent = content
-        .split("&").join("&amp;")
-        .split("<").join("&lt;")
-        .split(">").join("&gt;")
-        .split("█").join("&marker;")
-        // Bold or increased intensity
-        .split("\x1B[1m").join("<b>").split("\x1B[22m").join("</b>")
-        // Faint (decreased intensity)
-        .split("\x1B[2m").join("")
-        // Underline: Single
-        .split("\x1B[4m").join("<u>").split("\x1B[24m").join("</u>")
-        // Default text color (foreground)
-        .split("\x1B[39m").join("</span>")
-        // Reset / Normal
-        .split("\x1B[0m").join("</span>");
-    colors.forEach((name, index) => {
-        htmlContent = htmlContent
-            .split(`\x1B[3${index}m`).join(`<span class="${name}">`)
-            .split(`\x1B[3${index};1m`).join(`<span class="${name}">`)
-            .split(`\x1B[9${index}m`).join(`<span class="${name} bright">`);
-    });
-    return htmlContent;
-};
-*/
+const colors = {
+  1: '<b>',
+  22: '</b>',
+  4: '<u>',
+  24: '</u>',
+  39: '</span>',
+  0: '</span>'
+}
+
+const span = className => `<span class="${className}">`
+
+'black,red,green,yellow,blue,magenta,cyan,white'
+  .split(',')
+  .forEach((color, index) => {
+    colors[`3${index}`] = span(color)
+    colors[`3${index};1`] = span(color)
+    colors[`9${index}`] = span(`${color} bright`)
+  })
 
 module.exports = string => {
-  const htmlEntities = string
+  const html = string
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
     .replace(/█/g, '&marker;')
-  if (htmlEntities.includes('\x1B[')) {
-    return htmlEntities
-        // .replace(/\x1B\[)
-        // .        .split("\x1B[1m").join("<b>").split("\x1B[22m").join("</b>")
-
-
+  if (html.includes('\x1B[')) {
+    return html.replace(/\x1B\[([0-9;]+)m/g, (_, code) => colors[code] || '')
   }
-  return htmlEntities
+  return html
 }
