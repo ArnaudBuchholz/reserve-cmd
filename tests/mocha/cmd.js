a1f3405e763d00ebf45ea671df849c2d8076efde
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-01-22 22:14:01 -0500

    output escapes

--- tests/mocha/cmd.js
+++ tests/mocha/cmd.js
@@ -2,6 +2,17 @@
 
 require('colors')
 
+function c (string) {
+  return string.split('')
+    .map(char => {
+      if (char === '\x1B') {
+        return '\\x1B'
+      }
+      return char
+    })
+    .join('')
+}
+
 const param = process.argv.pop()
 const handlers = {
   simple () {
@@ -9,6 +20,15 @@ const handlers = {
   },
   rainbow () {
     console.log('rainbow'.rainbow)
+  },
+  rainbow_c () {
+    console.log(c('rainbow'.rainbow))
+  },
+  red () {
+    console.log('red'.red)
+  },
+  red_c () {
+    console.log(c('red'.red))
   }
 }
 
