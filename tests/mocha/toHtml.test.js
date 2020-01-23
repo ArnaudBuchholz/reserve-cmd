30f7ea376747c21370bd7670ca6ceedc280c7779
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-01-22 22:14:14 -0500

    test colors escapes

--- tests/mocha/toHtml.test.js
+++ tests/mocha/toHtml.test.js
@@ -1,5 +1,6 @@
 'use strict'
 
+require('colors')
 const assert = require('./assert')
 const toHtml = require('../../toHtml')
 
@@ -13,4 +14,27 @@ describe('toHtml', () => {
     assert(() => toHtml('<< safe & reliable >>') === '&lt;&lt; safe &amp; reliable &gt;&gt;')
   })
 
+  it('converts color escape to HTML', () => {
+    assert(() => toHtml('red'.red) === '<span class="red">red</span>')
+  })
+
+  it('converts colors escapes to HTML', () => {
+    assert(() => toHtml('red'.rainbow) === [
+      '<span class="red">r</span>',
+      '<span class="yellow">e</span>',
+      '<span class="green">d</span>'
+    ].join(''))
+  })
+
+  it('converts bold escape to HTML', () => {
+    assert(() => toHtml('bold'.bold) === '<b>bold</b>')
+  })
+
+  it('converts underline escape to HTML', () => {
+    assert(() => toHtml('___'.underline) === '<u>___</u>')
+  })
+
+  it('converts italic escape to HTML', () => {
+    assert(() => toHtml('abc'.italic) === '<i>abc</i>')
+  })
 })
