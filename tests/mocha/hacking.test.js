b376d904b58cad581a413656e4f734f7737b2082
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-01-29 13:05:40 -0500

    Test unsupported verbs

---
+++ tests/mocha/hacking.test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const assert = require('./assert')
+
+describe('hacking', () => {
+  let mocked
+
+  before(async () => {
+    mocked = await require('./setup')
+  })
+
+  'HEAD,POST,PUT,DELETE,CUSTOM'.split(',').forEach(verb => {
+    it(`prevents unauthorized verb use (${verb})`, () => mocked.request(verb, '/simple')
+      .then(response => {
+        assert(() => response.statusCode === 500)
+      })
+    )
+  })
+})
