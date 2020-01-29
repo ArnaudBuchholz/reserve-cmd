4e8b143bca2ae2adbcb7c115900d442ce8344083
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-01-29 13:05:49 -0500

    Test text output

--- tests/mocha/text.test.js
+++ tests/mocha/text.test.js
@@ -2,18 +2,29 @@
 
 const assert = require('./assert')
 
-describe('client', async () => {
+describe('text', function () {
+  this.timeout(5000) // Since a real process is triggered
   let mocked
 
   before(async () => {
     mocked = await require('./setup')
   })
 
-  it('returns text content', () => mocked.request('GET', '/cmd?param=simple', {
+  it('returns text content', () => mocked.request('GET', '/simple', {
     accept: 'text/plain'
   })
     .then(response => {
-      assert(() => response.toString() === 'simple')
+      assert(() => response.statusCode === 200)
+      assert(() => response.toString() === 'simple\n')
+    })
+  )
+
+  it('returns text content (formatted)', () => mocked.request('GET', '/rainbow', {
+    accept: 'text/plain'
+  })
+    .then(response => {
+      assert(() => response.statusCode === 200)
+      assert(() => response.toString().includes('\x1B'))
     })
   )
 })
