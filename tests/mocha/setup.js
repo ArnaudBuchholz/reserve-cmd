1a6bb94e0f57aab936dcb9f8a18a0cc4ba7bc251
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-01-31 12:50:00 -0500

    Adds HTML customization and error case

--- tests/mocha/setup.js
+++ tests/mocha/setup.js
@@ -6,6 +6,16 @@ module.exports = require('reserve/mock')({
     cmd: require('../../index.js')
   },
   mappings: [{
+    match: /\/error/,
+    cmd: 'hopefully-not-a-process'
+  }, {
+    match: /\/customized\/(.*)/,
+    cwd: __dirname,
+    'html-tracking': true,
+    'html-header': '<script src="header.js"></script>',
+    'html-footer': '<p>footer</p>',
+    cmd: 'node ../cmd.js `$1`'
+  }, {
     match: /\/(.*)/,
     cwd: __dirname,
     cmd: 'node ../cmd.js `$1`'
